423b2c475b76dfa01fc376b5a4aad77542cdc053
Miguel Saturno <msaturno2021@gmail.com>
2026-03-25 23:48:53 -0500

    add remote backend s3

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "6.38.0"
     }
   }
+  backend "s3" {
+    bucket         = "test-terraform-mhiaghi"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
