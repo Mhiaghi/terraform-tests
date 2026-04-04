56339f9f98a7952c3900150eecb10c2730beb052
Miguel Saturno <msaturno2021@gmail.com>
2026-04-04 12:43:31 -0500

    flask changes

--- software/backend/app.py
+++ software/backend/app.py
@@ -2,14 +2,14 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/login', methods = ["POST"])
+@app.route('/login', methods = ["POST"])
 def login():
     data = request.get_json()
     name = data.get("username")
     password = data.get("password")
     return jsonify({"message": f"Hola {name}, el login esta funcionando con la pass {password}"})
 
-@app.route('/api/signin', methods = ["POST"])
+@app.route('/signin', methods = ["POST"])
 def signin():
     data = request.get_json()
     name = data.get("username")
