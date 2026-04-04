1ec200b0fff6717180aeb70fac82a07bb07766e0
Miguel Saturno <msaturno2021@gmail.com>
2026-04-04 00:39:35 -0500

    change_flow

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,6 +92,7 @@ resource "aws_instance" "vm" {
               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
               echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
               apt-get update -y
+              apt-get upgrade -y
               apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
               systemctl start docker
               systemctl enable docker
