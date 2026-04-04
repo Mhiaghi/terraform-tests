b9212a502770caa16959e28aa0230c59b9123924
Miguel Saturno <msaturno2021@gmail.com>
2026-04-03 23:56:08 -0500

    New try

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,6 +88,7 @@ resource "aws_instance" "vm" {
               apt-get update -y
               apt-get upgrade -y
               apt-get install -y docker.io
+              apt-get install -y docker-compose-plugin
               systemctl start docker
               systemctl enable docker
               snap install amazon-ssm-agent --classic
